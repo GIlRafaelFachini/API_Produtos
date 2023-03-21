fbeb0de9d17e4e2c935b4bdc27a622379542824e
GilRafaelfachini <gilrafaelfachini@gmail.com>
2023-03-21 08:36:52 -0300

    first commit

---
+++ src/main/java/br/com/projeto/produtos/resources/UserResource.java
@@ -0,0 +1,22 @@
+package br.com.projeto.produtos.resources;
+
+import br.com.projeto.produtos.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+
+        User user = new User (1L,"Gil","gilrafaelfachini@gmail.com", "996626585" , "fiat147");
+        return ResponseEntity.ok ().body (user);
+
+    }
+
+
+}
