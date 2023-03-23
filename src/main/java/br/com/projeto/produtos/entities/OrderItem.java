245c4ef5e3f4e28196509e5600ad6e15ed557b60
GilRafaelfachini <gilrafaelfachini@gmail.com>
2023-03-23 14:32:52 -0300

    first commit

---
+++ src/main/java/br/com/projeto/produtos/entities/OrderItem.java
@@ -0,0 +1,14 @@
+package br.com.projeto.produtos.entities;
+
+import br.com.projeto.produtos.entities.pk.OrderItemPk;
+
+public class OrderItem {
+
+    private OrderItemPk id;
+
+    private Integer quantity
+
+
+
+
+}
--- src/main/java/br/com/projeto/produtos/entities/OrderItemPk.java
+++ src/main/java/br/com/projeto/produtos/entities/OrderItemPk.java
@@ -2,11 +2,20 @@ package br.com.projeto.produtos.entities.pk;
 
 import br.com.projeto.produtos.entities.Order;
 import br.com.projeto.produtos.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
+import java.io.Serializable;
 import java.util.Objects;
-
-public class OrderItemPk {
+@Embeddable
+public class OrderItemPk implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @ManyToOne
+    @JoinColumn(name = "order_id")
     private Order order;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
     private Product product;
 
     public Order getOrder() {
